ef5c3540ca83b2640a5068f8607c0ba56710e579
thanagonp <thanagonp@kkumail.com>
2025-03-19 16:51:16 +0700

    config api url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://c530-171-6-104-94.ngrok-free.app/api";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const CLOUDINARY_UPLOAD_URL = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_URL || "";
 export const CLOUDINARY_UPLOAD_PRESET = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET || "";
 export const CLOUDINARY_CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || "";
