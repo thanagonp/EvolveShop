2227c62d0c5d27e5db9648a96479d6c6ff0bf93d
thanagonp <thanagonp@kkumail.com>
2025-03-31 13:19:03 +0700

    edit productList grid

--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -31,7 +31,7 @@ export default function ProductList() {
 
   return (
     <motion.div
-    className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-4 w-full"
+    className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 w-full"
     initial={{ opacity: 0, y: 50 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.6, ease: "easeOut" }}
